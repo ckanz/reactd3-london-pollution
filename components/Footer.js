ff87e6e5de1db15e63a251134531c6966425a110
Clemens Anzmann <clemens.anzmann@gmail.com>
2017-07-21 14:20:12 +0100

    update readme

--- components/Footer.js
+++ components/Footer.js
@@ -18,7 +18,7 @@ class Footer extends React.Component {
         <hr style={dividerStyle} />
         <div style={footerTextStyle}>
           <p>Each pollutant is measured once per hour across the London Air Quality Network. Averages are calculated from these measurements.</p>
-          <p>This chart has been created using the <a href="https://github.com/react-d3" target="blank">react-d3 framework</a>. Data comes from the <a href="https://data.london.gov.uk/dataset/london-average-air-quality-levels" target="blank">London Datastore</a>. You can find the source code for this project on <a href="https://github.com/ckanz/reactd3-london-pollution" target="blank">Github</a>.</p>
+          <p>This chart has been created using the <a href="https://github.com/react-d3" target="blank">react-d3 framework</a>. The source of the data is the <a href="https://data.london.gov.uk/dataset/london-average-air-quality-levels" target="blank">London Datastore</a>. You can find the source code for this project on <a href="https://github.com/ckanz/reactd3-london-pollution" target="blank">Github</a>.</p>
         </div>
     </div>
     );
